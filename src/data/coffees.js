41d90368fab899d3d884d354999abfc1e46dabef
Katelyn Kim <46480709+KatelynKim@users.noreply.github.com>
2022-09-18 18:38:54 -0700

    Remove comments in moods.js

--- src/data/coffees.js
+++ src/data/coffees.js
@@ -1,43 +1,43 @@
 const coffees =[
     { 
-        "id": 1,
-        "name": "caramel macchiato"
+        id: 1,
+        name: "caramel macchiato"
     },
     { 
-        "id": 2,
-        "name": "vanilla latte"
+        id: 2,
+        name: "vanilla latte"
     },
     { 
-        "id": 3,
-        "name": "espresso"
+        id: 3,
+        name: "espresso"
     },
     { 
-        "id": 4,
-        "name": "flat white"
+        id: 4,
+        name: "flat white"
     },
     { 
-        "id": 5,
-        "name": "caffe mocha"
+        id: 5,
+        name: "caffe mocha"
     },
     { 
-        "id": 6,
-        "name": "white chocolate mocha"
+        id: 6,
+        name: "white chocolate mocha"
     },
     { 
-        "id": 7,
-        "name": "caffe misto"
+        id: 7,
+        name: "caffe misto"
     },
     { 
-        "id": 8,
-        "name": "cinamon dolce latte"
+        id: 8,
+        name: "cinamon dolce latte"
     },
     { 
-        "id": 9,
-        "name": "black coffee"
+        id: 9,
+        name: "black coffee"
     },
     { 
-        "id": 10,
-        "name": "americano"
+        id: 10,
+        name: "americano"
     }
 ]
 
--- src/data/moods.js
+++ src/data/moods.js
@@ -1,83 +1,73 @@
-// mocha 2
-// white chocolate mocha 3
-// caffee misto 1
-// vanilla latte 2
-// cinamon dolce latte 2
-// caramel macchiato 3
-// americano 0 (bitter)
-// flat white 0 (more bitter than latte)
-// espresso 0 (most bitter and more caffeine than black coffee)
-// black coffee 0 (bitter)
 const moods = [
     {
-        "id": 1,
-        "mood": "hurt",
-        "scores": []
+        id: 1,
+        mood: "hurt",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     },
     {
-        "id": 2,
-        "mood": "hateful",
-        "scores": [0, 0, 1, 0.5, 0]
+        id: 2,
+        mood: "hateful",
+        scores: [0, 0, 1, 0.5, 0, 1, 1, 1, 1, 1]
     },
     {
-        "id": 3,
-        "mood": "disappointed",
-        "scores": [1, 0.5, 0, 0.5, 1]
+        id: 3,
+        mood: "disappointed",
+        scores: [1, 0.5, 0, 0.5, 1, 1, 1, 1, 1, 1]
     },
     {
-        "id": 4,
-        "mood": "confused",
-        "scores": [1, 0.5, 0, 0.5, 1]
+        id: 4,
+        mood: "confused",
+        scores: [1, 0.5, 0, 0.5, 1, 1, 1, 1, 1, 1]
     },
     {
-        "id": 5,
-        "mood": "exhausted",
-        "scores": [0, 0, 1, 0.5, 0]
+        id: 5,
+        mood: "exhausted",
+        scores: [0, 0, 1, 0.5, 0, 1, 1, 1, 1, 1]
     },
     {
-        "id": 6,
-        "mood": "anxious",
-        "scores": [1, 0.5, 0, 0.5, 1]
+        id: 6,
+        mood: "anxious",
+        scores: [1, 0.5, 0, 0.5, 1, 1, 1, 1, 1, 1]
     },
     {
-        "id": 7,
-        "mood": "proud",
-        "scores": [1, 0.5, 0, 0.5, 1]
+        id: 7,
+        mood: "proud",
+        scores: [1, 0.5, 0, 0.5, 1, 1, 1, 1, 1, 1]
     },
     {
-        "id": 8,
-        "mood": "peaceful",
-        "scores": [0, 0, 1, 0.5, 0]
+        id: 8,
+        mood: "peaceful",
+        scores: [0, 0, 1, 0.5, 0, 1, 1, 1, 1, 1]
     },
     {
-        "id": 9,
-        "mood": "lonely",
-        "scores": [1, 0.5, 0, 0.5, 1]
+        id: 9,
+        mood: "lonely",
+        scores: [1, 0.5, 0, 0.5, 1, 1, 1, 1, 1, 1]
     },
     {
-        "id": 10,
-        "mood": "joyful",
-        "scores": [1, 0.5, 0, 0.5, 1]
+        id: 10,
+        mood: "joyful",
+        scores: [1, 0.5, 0, 0.5, 1, 1, 1, 1, 1, 1]
     },
     {
-        "id": 11,
-        "mood": "grateful",
-        "scores": [0, 0, 1, 0.5, 0]
+        id: 11,
+        mood: "grateful",
+        scores: [0, 0, 1, 0.5, 0, 1, 1, 1, 1, 1]
     },
     {
-        "id": 12,
-        "mood": "guilty",
-        "scores": [1, 0.5, 0, 0.5, 1]
+        id: 12,
+        mood: "guilty",
+        scores: [1, 0.5, 0, 0.5, 1, 1, 1, 1, 1, 1]
     },
     {
-        "id": 13,
-        "mood": "excited",
-        "scores": [1, 0.5, 0, 0.5, 1]
+        id: 13,
+        mood: "excited",
+        scores: [1, 0.5, 0, 0.5, 1, 1, 1, 1, 1, 1]
     },
     {
-        "id": 14,
-        "mood": "special",
-        "scores": [1, 0.5, 0, 0.5, 1]
+        id: 14,
+        mood: "special",
+        scores: [1, 0.5, 0, 0.5, 1, 1, 1, 1, 1, 1]
     }
 ]
 
