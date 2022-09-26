b4696feff5bbc9f09a96afbe1b0d1ceaec8fd7c5
Injun.Lee <ila19@sfu.ca>
2022-09-25 22:10:40 -0700

    Geolocation API Implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css'
-import getCurrentLocation from './lib/get-user-location'
 
 function App() {
-  const result = getCurrentLocation()
-  return (<div>Coffee Chaser</div>)
+  return <div>Coffee Chaser</div>
 }
 
 export default App
