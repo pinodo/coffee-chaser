13ed3fa16b6bdd57af0e9f0e9ac67528959f5d42
Injun.Lee <ila19@sfu.ca>
2022-10-09 17:25:00 -0700

    Fixed bugs that have been reviewed

--- src/data/coffees.js
+++ src/data/coffees.js
@@ -1,24 +1,24 @@
-const coffees =[
-    { 
-        "id": 1,
-        "name": "caramel macchiato"
-    },
-    { 
-        "id": 2,
-        "name": "vanilla latte"
-    },
-    { 
-        "id": 3,
-        "name": "espresso"
-    },
-    { 
-        "id": 4,
-        "name": "flat white"
-    },
-    { 
-        "id": 5,
-        "name": "mocha"
-    }
+const coffees = [
+  {
+    id: 1,
+    name: 'caramel macchiato'
+  },
+  {
+    id: 2,
+    name: 'vanilla latte'
+  },
+  {
+    id: 3,
+    name: 'espresso'
+  },
+  {
+    id: 4,
+    name: 'flat white'
+  },
+  {
+    id: 5,
+    name: 'mocha'
+  }
 ]
 
 export default coffees
--- src/data/moods.js
+++ src/data/moods.js
@@ -1,19 +1,19 @@
 const moods = [
-    {
-        "id": 1,
-        "mood": "sad",
-        "scores": [1, 0.5, 0, 0.5, 1]
-    },
-    {
-        "id": 2,
-        "mood": "tired",
-        "scores": [0, 0, 1, 0.5, 0]
-    },
-    {
-        "id": 3,
-        "mood": "anxious",
-        "scores": [1, 0.5, 0, 0.5, 1]
-    }  
+  {
+    id: 1,
+    mood: 'sad',
+    scores: [1, 0.5, 0, 0.5, 1]
+  },
+  {
+    id: 2,
+    mood: 'tired',
+    scores: [0, 0, 1, 0.5, 0]
+  },
+  {
+    id: 3,
+    mood: 'anxious',
+    scores: [1, 0.5, 0, 0.5, 1]
+  }
 ]
 
 export default moods
